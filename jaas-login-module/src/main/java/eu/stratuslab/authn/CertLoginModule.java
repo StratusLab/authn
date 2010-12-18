ef218defe5c1e8d6af7a965abce25a6b8e86c217
Charles Loomis <loomis@lal.in2p3.fr>
2010-12-18 14:16:54 +0100

    create separate module for login module

---
+++ jaas-login-module/src/main/java/eu/stratuslab/authn/CertLoginModule.java
@@ -0,0 +1,61 @@
+
+package eu.stratuslab.authn;
+
+import java.security.Principal;
+import java.util.Map;
+
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.Subject;
+import javax.security.auth.spi.LoginModule;
+
+import org.eclipse.jetty.plus.jaas.JAASRole;
+
+public class CertLoginModule implements LoginModule {
+
+    public boolean abort() {
+	return true;
+    }
+
+    public boolean commit() {
+	return true;
+    }
+
+    public void initialize(Subject subject, 
+			   CallbackHandler callbackHandler, 
+			   Map<String, ?> sharedState, 
+			   Map<String, ?> options) {
+
+	System.err.println("Adding 'cloud-access' role...");
+	JAASRole role = new JAASRole("cloud-access");
+	subject.getPrincipals().add(role);
+
+	System.err.println("Principals:");
+	for (Principal p : subject.getPrincipals()) {
+	    System.err.println(p.getName());
+	}
+
+	System.err.println("Shared State:");
+	for (String key : sharedState.keySet()) {
+	    Object value = sharedState.get(key);
+	    System.err.println(key + " => " + value);
+	}
+
+	System.err.println("\nOptions:");
+	for (String key : options.keySet()) {
+	    Object value = options.get(key);
+	    System.err.println(key + " => " + value);
+	}
+	System.err.println("\n");
+
+    }
+
+    public boolean login() {
+	return true;
+    }
+
+    public boolean logout() {
+	return true;
+    }
+
+}
+
