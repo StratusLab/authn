eed98fed4a0bf89ca404bad09a19f1d798b259d9
Charles Loomis <loomis@lal.in2p3.fr>
2010-12-18 14:46:49 +0100

    add eclipse files; reformat code

--- jaas-login-module/src/main/java/eu/stratuslab/authn/CertLoginModule.java
+++ jaas-login-module/src/main/java/eu/stratuslab/authn/CertLoginModule.java
@@ -1,11 +1,11 @@
-
 package eu.stratuslab.authn;
 
 import java.security.Principal;
 import java.util.Map;
+import java.util.Map.Entry;
 
-import javax.security.auth.callback.CallbackHandler;
 import javax.security.auth.Subject;
+import javax.security.auth.callback.CallbackHandler;
 import javax.security.auth.spi.LoginModule;
 
 import org.eclipse.jetty.plus.jaas.JAASRole;
@@ -13,49 +13,44 @@ import org.eclipse.jetty.plus.jaas.JAASRole;
 public class CertLoginModule implements LoginModule {
 
     public boolean abort() {
-	return true;
+        return true;
     }
 
     public boolean commit() {
-	return true;
+        return true;
     }
 
-    public void initialize(Subject subject, 
-			   CallbackHandler callbackHandler, 
-			   Map<String, ?> sharedState, 
-			   Map<String, ?> options) {
+    public void initialize(Subject subject, CallbackHandler callbackHandler,
+            Map<String, ?> sharedState, Map<String, ?> options) {
 
-	System.err.println("Adding 'cloud-access' role...");
-	JAASRole role = new JAASRole("cloud-access");
-	subject.getPrincipals().add(role);
+        System.err.println("Adding 'cloud-access' role...");
+        JAASRole role = new JAASRole("cloud-access");
+        subject.getPrincipals().add(role);
 
-	System.err.println("Principals:");
-	for (Principal p : subject.getPrincipals()) {
-	    System.err.println(p.getName());
-	}
+        System.err.println("Principals:");
+        for (Principal p : subject.getPrincipals()) {
+            System.err.println(p.getName());
+        }
 
-	System.err.println("Shared State:");
-	for (String key : sharedState.keySet()) {
-	    Object value = sharedState.get(key);
-	    System.err.println(key + " => " + value);
-	}
+        System.err.println("Shared State:");
+        for (Entry<String, ?> entry : sharedState.entrySet()) {
+            System.err.println(entry.getKey() + " => " + entry.getValue());
+        }
 
-	System.err.println("\nOptions:");
-	for (String key : options.keySet()) {
-	    Object value = options.get(key);
-	    System.err.println(key + " => " + value);
-	}
-	System.err.println("\n");
+        System.err.println("\nOptions:");
+        for (Entry<String, ?> entry : options.entrySet()) {
+            System.err.println(entry.getKey() + " => " + entry.getValue());
+        }
+        System.err.println("\n");
 
     }
 
     public boolean login() {
-	return true;
+        return true;
     }
 
     public boolean logout() {
-	return true;
+        return true;
     }
 
 }
-
