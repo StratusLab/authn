464f7b67952d961b149505986cd76d61201743b0
Charles Loomis <cal@sixsq.com>
2012-03-08 21:05:16 +0100

    add constructor to grid ssl connector; update configuration

--- ssl/src/main/java/eu/stratuslab/ssl/GridSslSelectChannelConnector.java
+++ ssl/src/main/java/eu/stratuslab/ssl/GridSslSelectChannelConnector.java
@@ -23,22 +23,28 @@ package eu.stratuslab.ssl;
 import java.security.Security;
 
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.eclipse.jetty.http.ssl.SslContextFactory;
 import org.eclipse.jetty.server.ssl.SslSelectChannelConnector;
 
 public class GridSslSelectChannelConnector extends SslSelectChannelConnector {
 
-    @Override
-    public void doStart() throws Exception {
+	public GridSslSelectChannelConnector(SslContextFactory sslContextFactory) {
+		super(sslContextFactory);
+	}
 
-        // Add the BouncyCastle (crypto algorithms) and TrustManager providers.
-        if (Security.getProvider(GridTrustManagerProvider.PROVIDER_NAME) == null) {
-            Security.addProvider(new GridTrustManagerProvider());
-        }
-        if (Security.getProvider(BouncyCastleProvider.PROVIDER_NAME) == null) {
-            Security.addProvider(new BouncyCastleProvider());
-        }
+	@Override
+	public void doStart() throws Exception {
 
-        super.doStart();
+		// Add the BouncyCastle (crypto algorithms) and TrustManager providers.
+		if (Security.getProvider(GridTrustManagerProvider.PROVIDER_NAME) == null) {
+			Security.addProvider(new GridTrustManagerProvider());
+		}
+		if (Security.getProvider(BouncyCastleProvider.PROVIDER_NAME) == null) {
+			Security.addProvider(new BouncyCastleProvider());
+		}
 
-    }
+		super.doStart();
+
+	}
+
 }
