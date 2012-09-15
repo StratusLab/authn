36523f6ba805cf19d98644f0a26a2c4c1d99fb64
Charles Loomis <cal@sixsq.com>
2012-09-15 18:29:32 +0200

    add enlive for html generation; use for simple login and status pages

--- vm-rest-api/src/main/clojure/eu/stratuslab/authn/vm_rest/views/authn.clj
+++ vm-rest-api/src/main/clojure/eu/stratuslab/authn/vm_rest/views/authn.clj
@@ -2,6 +2,7 @@
   (:require [noir.core :refer [defpage pre-route]]
             [noir.response :as resp]
             [cemerick.friend :as friend]
+            [net.cgrand.enlive-html :refer [deftemplate content]]
             [clojure.tools.logging :as log]))
 
 (def context-path (atom ""))
@@ -9,31 +10,6 @@
 (defn set-context-path
   [path]
   (reset! context-path path))
-
-;;
-;; Leave out the action attribute so that the URL defaults to the same
-;; URL as the initial form.  This avoids needing to add the servlet
-;; context path explicitly.
-;;
-;; FIXME: Should be done with enlive.
-;;
-(def ^:const login-html
-     "<!DOCTYPE html>
-<html>
-  <head>
-    <title>StratusLab VM Management</title>
-  </head>
-  <body>
-    <form method='POST'>
-      <label for='username'>Username: </label>
-      <input id='username' name='username' type='text' />
-      <label for='password'>Password: </label>
-      <input id='password' name='password' type='password' />
-      <input type='submit' value='Login'>
-    </form>
-  </body>
-</html>
-")
 
 (defn clear-identity [response]
   (update-in response [:session] dissoc ::identity))
@@ -44,8 +20,11 @@
             ;; no-op
             nil))
 
+(deftemplate login "eu/stratuslab/authn/vm_rest/views/login.html" []
+  [:div#wrapper :p] (content "Please login to access services."))
+
 (defpage "/login" {}
-  login-html)
+  (apply str (login)))
 
 (defpage "/logout" {}
   (clear-identity (resp/redirect (str @context-path "/"))))
